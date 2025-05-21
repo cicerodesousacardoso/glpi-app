1207ff1e82ae60e738c384645d2d98f558e6ca67
cicerodesousacardoso <cicerocardoso@aluno.uema.br>
2025-05-21 11:17:08 -0300

    Segundo commit

--- database/migrations/2025_05_21_131442_create_tickets_table.php
+++ database/migrations/2025_05_21_131442_create_tickets_table.php
@@ -6,22 +6,19 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('tickets', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('tickets');
     }
 };
