5503d315924e72c261533b15569dad214f2254cf
Cícero Cardoso <cicerodesousacardoso@gmail.com>
2025-05-28 17:38:20 -0300

    Alterações nos seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,8 @@ class DatabaseSeeder extends Seeder
 {
     $this->call([
         RolesTableSeeder::class,
-        // Outros seeders aqui...
+        AdminUserSeeder::class, // <-- Adicione esta linha
+    
     ]);
 }
 
